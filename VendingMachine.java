a5f5c6608bf53d7fb45577a0a815fb981ca5892c
khryssha <127598730+khryssha@users.noreply.github.com>
2023-06-28 01:15:07 +0800

    Update VendingMachine.java
    Added insertMoney method

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class VendingMachine {
     Slot slots[] = new Slot[8];
 
@@ -44,7 +46,34 @@ public class VendingMachine {
     }
 
     public int insertMoney() {
+        Scanner sc = new Scanner(System.in);
+        int totalInp = 0;
+        int temp = 0;
 
+        System.out.print("Insert money (1 5 10 20 50 100): ");
+        String moneyInp = sc.nextLine();
+
+        String[] moneyArr = moneyInp.split(" ");
+
+        for (int i = 0; i < moneyArr.length; i++){
+            temp = Integer.parseInt(moneyArr[i]);
+            switch (temp){
+                case 1:
+                case 5:
+                case 10:
+                case 20:
+                case 50:
+                case 100:
+                case 200:
+                case 500:
+                case 1000:
+                    totalInp = totalInp + temp; break;
+                default:
+                    System.out.println(temp + " is an invalid denomination.");
+            }
+        }
+        System.out.println("Total money inserted: " + totalInp);
+        return totalInp;
     }
 
 }
