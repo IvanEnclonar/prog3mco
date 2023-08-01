7d77bffd99618af3631089f513e89eeba2ca45bb
khryssha <127598730+khryssha@users.noreply.github.com>
2023-08-01 09:32:40 +0800

    Create FactoryController.java
    Controller file of the factory itself

---
+++ FactoryController.java
@@ -0,0 +1,80 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FactoryController{
+    private FactoryView factoryView;
+    private Model model;
+
+    public FactoryController(FactoryView factoryView, Model model){
+        this.factoryView = factoryView;
+        this.model = model;
+
+        factoryView.createRVMListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(model.vmExists()){
+                    if(model.isSpecial()){
+                        factoryView.display("A Special Vending Machine already exists.");
+                    }
+                    else{
+                        factoryView.display("A Regular Vending Machine already exists.");
+                    }
+                }
+                else if(model.createRegularVM()){
+                    factoryView.display("Regular Vending Machine created.");
+                }
+                else{
+                    factoryView.display("Error creating vending machine.");
+
+                }
+            }
+        });
+
+        factoryView.createSVMListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(model.vmExists()){
+                    if(model.isSpecial()) {
+                        factoryView.display("A Special Vending Machine already exists.");
+                    }
+                    else{
+                        factoryView.display("A Regular Vending Machine already exists.");
+                    }
+                }
+                else if(model.createSpecialVM()){
+                    factoryView.display("Special Vending Machine created.");
+                }
+                else{
+                    factoryView.display("Error creating vending machine.");
+
+                }
+            }
+        });
+
+        factoryView.testVMListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(model.vmExists()){
+                    if(model.isSpecial()){
+                        factoryView.display("Testing Special Vending Machine.");
+                    }
+                    else{
+                        factoryView.display("Testing Regular Vending Machine.");
+                        RegularView rvm = new RegularView();
+                        RegularController rvmController = new RegularController(rvm, model);
+                    }
+                }
+                else{
+                    factoryView.display("No Vending Machine exists.");
+                }
+            }
+        });
+
+        factoryView.maintainVMListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+    }
+}
